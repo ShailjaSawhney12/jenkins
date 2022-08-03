e5ee91321b20b7cc9e0f092a06e7e15d8b6c8df0
shailjasawhney <shailja.sawhney@fullstacklabs.ca>
2022-08-03 09:29:28 -0400

    minor changes

--- src/main/java/com/example/jenkinsdemo/controllers/SampleController.java
+++ src/main/java/com/example/jenkinsdemo/controllers/SampleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
 
-    @GetMapping
+    @GetMapping("/hello")
     public String message(){
         return "Welcome to java techie!!!!";
     }
