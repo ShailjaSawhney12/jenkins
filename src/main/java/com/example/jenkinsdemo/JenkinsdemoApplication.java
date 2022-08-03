1d2c1d8289273eaeb43a882248e06f73f7b80e09
shailjasawhney <shailja.sawhney@fullstacklabs.ca>
2022-08-03 10:52:24 -0400

    minor changes

--- src/main/java/com/example/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/example/jenkinsdemo/JenkinsdemoApplication.java
@@ -2,10 +2,17 @@ package com.example.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsdemoApplication {
 
+	@GetMapping("/hello")
+	public String message(){
+		return "Welcome to java techie!!!!";
+	}
 	public static void main(String[] args) {
 
 
--- src/main/java/com/example/jenkinsdemo/controllers/SampleController.java
+++ src/main/java/com/example/jenkinsdemo/controllers/SampleController.java
@@ -4,12 +4,12 @@ package com.example.jenkinsdemo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class SampleController {
 
 
-    @GetMapping("/hello")
-    public String message(){
-        return "Welcome to java techie!!!!";
-    }
+//    @GetMapping("/hello")
+//    public String message(){
+//        return "Welcome to java techie!!!!";
+//    }
 }
